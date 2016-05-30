179d0db7dc568c6cd33018cdd934fb5232dd2022
jerabaul29 <jean.rblt@gmail.com>
2016-05-30 19:39:59 +0200

    Added all files

--- variants/arduino_due_paddle/pins_arduino.h
+++ variants/arduino_due_paddle/pins_arduino.h
@@ -16,27 +16,6 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "RingBuffer_1_RX.h"
-#include <string.h>
+// API compatibility
+#include "variant.h"
 
-RingBuffer_1_RX::RingBuffer_1_RX( void )
-{
-    memset( (void *)_aucBuffer, 0, SERIAL_BUFFER_SIZE_1_RX ) ;
-    _iHead=0 ;
-    _iTail=0 ;
-}
-
-void RingBuffer_1_RX::store_char( uint8_t c )
-{
-  int i = (uint32_t)(_iHead + 1) % SERIAL_BUFFER_SIZE_1_RX ;
-
-  // if we should be storing the received character into the location
-  // just before the tail (meaning that the head would advance to the
-  // current location of the tail), we're about to overflow the buffer
-  // and so we don't write the character or advance the head.
-  if ( i != _iTail )
-  {
-    _aucBuffer[_iHead] = c ;
-    _iHead = i ;
-  }
-}
